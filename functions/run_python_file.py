506faf637acc07f8a5cf784d51fd359cf96df1b4
Luukiii <lukasz.kurdziel99@gmail.com>
2025-09-25 18:54:08 +0200

    fixes

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -6,7 +6,7 @@ def run_python_file(working_dir, file_path, args=[]):
     abs_access_file = os.path.abspath(os.path.join(working_dir, file_path))
     
     if not os.path.commonprefix([abs_working_dir, abs_access_file]) == abs_working_dir:
-        return f'Error: Cannot list "{abs_access_file}" as it is outside the permitted working directory'
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     
     if not os.path.isfile(abs_access_file):
         return f'Error: File "{file_path}" not found.'
@@ -14,18 +14,15 @@ def run_python_file(working_dir, file_path, args=[]):
     if not file_path.endswith(".py"):
         return f'Error: "{file_path}" is not a Python file.'
 
-    stdout = ""
-    stderr = ""
-
     try:
         process = subprocess.run(["python3", abs_access_file, *args], capture_output=True, timeout=30)
 
         process_status = ""
-        if len(process.stdout) != 0:
+        if process.stdout != None:
             process_status = f"STDOUT: {process.stdout}, "
 
-        if len(process.stderr) != 0:
-            process_status = f"STDERR: {process.stderr}, "
+        if process.stderr != None:
+            process_status = process_status + f"STDERR: {process.stderr}, "
 
         if process.returncode != 0:
             return f"Process exited with code {process.returncode}"
